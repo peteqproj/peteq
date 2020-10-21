a4c77ff826f37d91adb1c95c0f7cd112db355de9
Oleg Sucharevich <oleg2807@gmail.com>
2020-10-21 20:53:58 +0300

    feat: update rabbitmq pkg with replay usecases

--- pkg/event/bus/rabbitmq/rabbitmq.go
+++ pkg/event/bus/rabbitmq/rabbitmq.go
@@ -6,9 +6,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
+	retry "github.com/avast/retry-go"
 	"github.com/doug-martin/goqu/v9"
 	"github.com/doug-martin/goqu/v9/exp"
 	"github.com/gofrs/uuid"
@@ -39,6 +41,10 @@ type (
 // Publish event
 func (e *Eventbus) Publish(ctx context.Context, ev event.Event, done chan<- error) string {
 	e.Logger.Info("Publishing event", "name", ev.Metadata.Name)
+	if err := e.ensureQueue(e.getKey(ev), false); err != nil {
+		done <- fmt.Errorf("Failed to ensure queue: %w", err)
+		return ""
+	}
 	id, err := uuid.NewV4()
 	if err != nil {
 		e.Logger.Info("Failed to create event id", "error", err.Error())
@@ -52,8 +58,7 @@ func (e *Eventbus) Publish(ctx context.Context, ev event.Event, done chan<- erro
 		done <- err
 		return ""
 	}
-	q := e.getQueue(ev)
-	if err := e.publish(ev.Metadata.Name, q, bytes); err != nil {
+	if err := e.publish(ev.Metadata.Name, e.getKey(ev), bytes); err != nil {
 		e.Logger.Info("Failed to publish event", "event", ev.Metadata.Name, "error", err.Error())
 		done <- err
 		return ""
@@ -140,18 +145,40 @@ func (e *Eventbus) Replay(ctx context.Context) error {
 		}
 		set = append(set, event)
 	}
+	if len(set) == 0 {
+		e.Logger.Info("No events were found")
+		return nil
+	}
+	key := fmt.Sprintf("replay-%s", e.getKey(set[0]))
+	if err := e.ensureQueue(key, true); err != nil {
+		return fmt.Errorf("Failed to ensure queue: %w", err)
+	}
 	for _, ev := range set {
 		e.Logger.Info("Processing event", "name", ev.Metadata.Name)
-		q := e.getQueue(ev)
 		data, err := json.Marshal(ev)
 		if err != nil {
 			return err
 		}
-		if err := e.publish(ev.Metadata.Name, q, data); err != nil {
+		if err := e.publish(ev.Metadata.Name, key, data); err != nil {
 			return err
 		}
 	}
-	return nil
+	for {
+		queue, err := e.Channel.QueueInspect(key)
+		if err != nil {
+			return err
+		}
+		if queue.Messages != 0 {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		e.Logger.Info("No messages in queue, deleting")
+		_, err = e.Channel.QueueDelete(key, false, true, false)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
 }
 
 func (e *Eventbus) start() error {
@@ -168,11 +195,9 @@ func (e *Eventbus) start() error {
 	return nil
 }
 
-func (e *Eventbus) publish(name string, queue string, data []byte) error {
-	if err := e.ensureQueue(queue); err != nil {
-		return fmt.Errorf("Failed to ensure queue: %w", err)
-	}
-	err := e.Channel.Publish("", queue, false, false, amqp.Publishing{
+func (e *Eventbus) publish(name string, key string, data []byte) error {
+	defaultExchange := ""
+	err := e.Channel.Publish(defaultExchange, key, false, false, amqp.Publishing{
 		ContentType: "text/plain",
 		Body:        data,
 	})
@@ -182,7 +207,7 @@ func (e *Eventbus) publish(name string, queue string, data []byte) error {
 	return nil
 }
 
-func (e *Eventbus) getQueue(ev event.Event) string {
+func (e *Eventbus) getKey(ev event.Event) string {
 	return fmt.Sprintf("user-%s", ev.Tenant.ID)
 }
 
@@ -229,32 +254,32 @@ func (e *Eventbus) queueList() map[string]bool {
 func (e *Eventbus) watchQueues() {
 	knownQueues := map[string]bool{}
 	for {
+		e.Logger.Info("Listing queues")
 		list := e.queueList()
 		for name := range list {
+			e.Logger.Info("Testing queue for consumptions", "name", name)
 			if _, ok := knownQueues[name]; !ok {
 				e.Logger.Info("Queue added, starting to watch", "name", name)
+				replayQueue := false
+				if strings.HasPrefix(name, "replay") {
+					replayQueue = true
+				}
+				msgs, err := e.Channel.Consume(name, "", false, replayQueue, false, false, nil)
+				if err != nil {
+					e.Logger.Info("Failed to Consume", "error", err.Error())
+					continue
+				}
 				knownQueues[name] = true
-				go e.watchQueue(name, e.Logger.Fork("queue", name))
+				go e.watchQueue(msgs, e.Logger.Fork("queue", name))
 			}
 		}
 		time.Sleep(5 * time.Second)
 	}
 }
 
-func (e *Eventbus) watchQueue(q string, lgr logger.Logger) {
-	if err := e.ensureQueue(q); err != nil {
-		lgr.Info(err.Error())
-		// TODO: use channel to report back error
-		return
-	}
-	if err := e.ensureQueue(q); err != nil {
-		e.Logger.Info("Failed to ensure channel", "error", err.Error())
-	}
-	msgs, err := e.Channel.Consume(q, "", false, true, false, false, nil)
-	if err != nil {
-		lgr.Info("Failed to Consume", "error", err.Error())
-	}
-	for msg := range msgs {
+func (e *Eventbus) watchQueue(ch <-chan amqp.Delivery, lgr logger.Logger) {
+
+	for msg := range ch {
 		ev := event.Event{}
 		err := json.Unmarshal(msg.Body, &ev)
 		if err != nil {
@@ -267,25 +292,44 @@ func (e *Eventbus) watchQueue(q string, lgr logger.Logger) {
 			lgr.Info("Handler not found", "event", ev.Metadata.Name)
 			continue
 		}
-		lgr.Info("Calling event handlers", "len", len(set))
+		wg := &sync.WaitGroup{}
 		for _, h := range set {
+			wg.Add(1)
 			lgr.Info("Calling event handler", "name", h.Name())
 			log := logger.New(logger.Options{})
-			if err := h.Handle(context.Background(), ev, log.Fork("event", ev.Metadata.Name, "handler", h.Name())); err != nil {
-				lgr.Info("Failed to handle event", "event", ev.Metadata.Name, "handler", h.Name(), "error", err.Error())
-			}
+			elgr := log.Fork("event", ev.Metadata.Name, "id", ev.Metadata.ID, "handler", h.Name())
+			go e.deliverEvent(context.Background(), wg, ev, h, elgr)
 		}
+		wg.Wait()
 		if err := e.Channel.Ack(msg.DeliveryTag, false); err != nil {
 			e.Logger.Info("Failed to ack evenet", "error", err.Error())
 		}
 	}
 }
 
-func (e *Eventbus) ensureQueue(name string) error {
+func (e *Eventbus) deliverEvent(ctx context.Context, wg *sync.WaitGroup, ev event.Event, handler handler.EventHandler, lgr logger.Logger) {
+	delay := retry.DelayType(func(n uint, err error, config *retry.Config) time.Duration {
+		lgr.Info("Failed to handle event, retrying")
+		return retry.BackOffDelay(n, err, config)
+	})
+	if err := retry.Do(func() error {
+		return handler.Handle(ctx, ev, lgr)
+	}, retry.Delay(1*time.Second), retry.Attempts(5), delay); err != nil {
+		lgr.Info("Failed to handle event", "error", err.Error())
+	}
+	wg.Done()
+}
+
+func (e *Eventbus) ensureQueue(name string, replayQueue bool) error {
 	durable := false
 	autoDelete := true
 	exclusive := false
+	if replayQueue == true {
+		autoDelete = false
+		exclusive = false
+	}
 	noWait := false
+	e.Logger.Info("Creating queue", "autoDelete", autoDelete, "exclusive", exclusive)
 	_, err := e.Channel.QueueDeclare(name, durable, autoDelete, exclusive, noWait, nil)
 	if err != nil {
 		return fmt.Errorf("Failed to QueueDeclare: %w", err)
