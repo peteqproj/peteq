7eebff99d73436fc341e763eafe8301a09469b0b
Oleg Sucharevich <oleg2807@gmail.com>
2021-02-21 19:50:08 +0200

    add rss-import command

--- cmd/import-rss.go
+++ cmd/import-rss.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Pallinder/go-randomdata"
 	"github.com/mmcdole/gofeed"
 	"github.com/peteqproj/peteq/pkg/client"
 	"github.com/peteqproj/peteq/pkg/logger"
@@ -11,7 +12,8 @@ import (
 )
 
 var importRSSFlags struct {
-	url string
+	url     string
+	project string
 }
 
 var importRSSCmd = &cobra.Command{
@@ -29,12 +31,38 @@ var importRSSCmd = &cobra.Command{
 			return err
 		}
 		c := client.NewAPIClient(cnf)
-
+		projectID := ""
+		if importRSSFlags.project != "" {
+			logr.Info("Requesting projects", "name", importRSSFlags.project)
+			projects, _, err := c.RestAPIApi.ApiProjectGet(auth).Execute()
+			if err != nil {
+				return err
+			}
+			for _, p := range projects {
+				if *p.Metadata.Name == importRSSFlags.project {
+					logr.Info("Project matched to ID", "id", *p.Metadata.Id, "name", *p.Metadata.Name)
+					projectID = *p.Metadata.Id
+					break
+				}
+			}
+			// project not found - create one
+			if projectID == "" {
+				resp, _, err := c.ProjectCommandAPIApi.CProjectCreatePost(auth).Body(client.CreateProjectRequestBody{
+					Name:     importRSSFlags.project,
+					Color:    utils.PtrString(colors[randomdata.Number(0, len(colors))]),
+					ImageUrl: &rss.Image.URL,
+				}).Execute()
+				if err != nil {
+					return err
+				}
+				projectID = *resp.Id
+			}
+		}
 		for _, i := range rss.Items {
-			fmt.Println(i.Title)
 			_, _, err := c.TaskCommandAPIApi.CTaskCreatePost(auth).Body(client.CreateTaskResponse{
 				Name:        i.Title,
 				Description: utils.PtrString(fmt.Sprintf("Link: %s\n%s", i.Link, i.Description)),
+				Project:     utils.PtrString(projectID),
 			}).Execute()
 			if err != nil {
 				return err
@@ -48,4 +76,5 @@ var importRSSCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(importRSSCmd)
 	importRSSCmd.Flags().StringVar(&importRSSFlags.url, "url", "", "URL of the RSS")
+	importRSSCmd.Flags().StringVar(&importRSSFlags.project, "project", "", "Assign all tasks to given project (create the project if not exists)")
 }
