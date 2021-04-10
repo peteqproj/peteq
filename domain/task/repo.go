ca1eaaaf4a4193ba08ec2367f5fbb05708832ca0
Oleg Sucharevich <oleg2807@gmail.com>
2021-04-10 17:57:19 +0300

    generate task repo

--- domain/task/repo.go
+++ domain/task/repo.go
@@ -2,45 +2,250 @@ package task
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 
+	"github.com/peteqproj/peteq/domain/user"
+	
+	"github.com/doug-martin/goqu/v9"
+	"github.com/doug-martin/goqu/v9/exp"
 	"github.com/peteqproj/peteq/pkg/db"
 	"github.com/peteqproj/peteq/pkg/logger"
+	"github.com/peteqproj/peteq/pkg/repo"
+	"github.com/peteqproj/peteq/pkg/tenant"
 )
 
 const db_name = "repo_task"
 
-var errNotFound = errors.New("Resource not found")
+var errNotFound = errors.New("Task not found")
+var errNotInitiated = errors.New("Repository was not initialized, make sure to call Initiate function")
+var errNoTenantInContext = errors.New("No tenant in context")
+var queries = []string{
+	"CREATE TABLE IF NOT EXISTS repo_task ( id text not null,user text not null,info json not null,PRIMERY KEY (id));",
+	"CREATE INDEX user ON repo_task ( user);",
+}
 
 type (
 	Repo struct {
-		DB     db.Database
-		Logger logger.Logger
+		DB        db.Database 
+		Logger    logger.Logger
+		Initiated bool
+		Def       repo.RepoDef
 	}
 
-	ListOptions struct{}
-	GetOptions  struct {
+	ListOptions struct {}
+	GetOptions struct {
 		ID    string
 		Query string
 	}
 )
-
-func (r *Repo) List(ctx context.Context, options ListOptions) ([]*Task, error) {
-	return nil, nil
+func (r *Repo) Initiate(ctx context.Context) error {
+    for _, q := range queries {
+		r.Logger.Info("Running db init query", "query", q)
+		if _, err := r.DB.ExecContext(ctx, q); err != nil {
+			return err
+		}
+	}
+	r.Initiated = true
+	return nil
 }
+/* PrimeryKey functions*/
 
-func (r *Repo) Get(ctx context.Context, options GetOptions) (*Task, error) {
-	return nil, nil
-}
+func (r *Repo) Create(ctx context.Context, resource *Task) error {
+    if !r.Initiated {
+		return errNotInitiated
+	}
+	var user *user.User 
+	
+	if r.Def.Tenant != "" {
+		u := tenant.UserFromContext(ctx)
+		if u == nil {
+			return errNoTenantInContext
+		}
+		user = u
+	}
 
-func (r *Repo) Create(ctx context.Context, tresource *Task) error {
+	table_column_id := resource.Metadata.ID
+	table_column_user := user.Metadata.ID
+	table_column_info, err := json.Marshal(resource)
+	if err != nil {
+		return err
+	}
+	q, _, err := goqu.
+		Insert(db_name).
+		Cols(
+		"id",
+		"user",
+		"info",
+		).
+		Vals(goqu.Vals{
+		string(table_column_id),
+		string(table_column_user),
+		string(table_column_info),
+		}).
+		ToSQL()
+	if err != nil {
+		return err
+	}
+	_, err = r.DB.ExecContext(ctx, q)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (r *Repo) Delete(ctx context.Context, id string) error {
+func (r *Repo) GetById(ctx context.Context, id string) (*Task, error) {
+    if !r.Initiated {
+		return nil, errNotInitiated
+	}
+	e := exp.Ex{}
+	e["id"] = id
+	if r.Def.Tenant != "" {
+		u := tenant.UserFromContext(ctx)
+		if u == nil {
+			return nil, errNoTenantInContext
+		}
+		e["user"] = u.Metadata.ID
+	}
+	sql, _, err := goqu.From(db_name).Where(e).ToSQL()
+	if err != nil {
+		return nil, err
+	}
+	row := r.DB.QueryRowContext(ctx, sql)
+	if row.Err() != nil {
+		return nil, err
+	}
+	var table_id string
+	var table_user string
+	var table_info string
+	
+	if err := row.Scan(
+		&table_id,
+		&table_user,
+		&table_info,
+		); err != nil {
+		return nil, err
+	}
+	resource := &Task{}
+	// info column must exist
+	if err := json.Unmarshal([]byte(table_info), resource); err != nil {
+		return nil, err
+	}   
+	return resource, nil
+}
+func (r *Repo) UpdateTask(ctx context.Context, resource *Task) (error) {
+    if !r.Initiated {
+		return errNotInitiated
+	}
+	var user *user.User 
+	
+	if r.Def.Tenant != "" {
+		u := tenant.UserFromContext(ctx)
+		if u == nil {
+			return errNoTenantInContext
+		}
+		user = u
+	}
+
+	table_column_id := resource.Metadata.ID
+	table_column_user := user.Metadata.ID
+	table_column_info, err := json.Marshal(resource)
+	if err != nil {
+		return err
+	}
+	q, _, err := goqu.
+		Update(db_name).
+		Where(exp.Ex{
+			"id": resource.Metadata.ID,
+		}).
+		Set(goqu.Record{
+		"id": string(table_column_id),
+		"user": string(table_column_user),
+		"info": string(table_column_info),
+		}).
+		ToSQL()
+	if err != nil {
+		return err
+	}
+	_, err = r.DB.ExecContext(ctx, q)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+func (r *Repo) DeleteById(ctx context.Context, id string) (error) {
+	if !r.Initiated {
+		return errNotInitiated
+	}
+	e := exp.Ex{}
+	e["id"] = id
+	if r.Def.Tenant != "" {
+		u := tenant.UserFromContext(ctx)
+		if u == nil {
+			return errNoTenantInContext
+		}
+		e["user"] = u.Metadata.ID
+	}
+	q, _, err := goqu.
+		Delete(db_name).
+		Where(e).
+		ToSQL()
+	if err != nil {
+		return err
+	}
+	_, err = r.DB.ExecContext(ctx, q)
+	return err
+}
+/*End of PrimeryKey functions*/
+
+/*Index functions*/
+
+func (r *Repo) ListByUser(ctx context.Context, user string) ( []*Task, error) {
+	if !r.Initiated {
+		return nil, errNotInitiated
+	}
+	e := exp.Ex{}
+	e["user"] = user
+	if r.Def.Tenant != "" {
+		u := tenant.UserFromContext(ctx)
+		if u == nil {
+			return nil, errNoTenantInContext
+		}
+		e["user"] = u.Metadata.ID
+	}
+	sql, _, err := goqu.From(db_name).Where(e).ToSQL()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := r.DB.QueryContext(ctx, sql)
+	if err != nil {
+		return nil, err
+	}
+	res := []*Task{}
+	for rows.Next() {
+		var table_id string
+		var table_user string
+		var table_info string
+		
+		if err := rows.Scan(
+			&table_id,
+			&table_user,
+			&table_info,
+			); err != nil {
+			return nil, err
+		}
+		resource := &Task{}
+		// info column must exist
+		if err := json.Unmarshal([]byte(table_info), resource); err != nil {
+			return nil, err
+		}
+		res = append(res, resource)
+	}
+	return res, rows.Close()
+
 }
 
-func (r *Repo) Update(ctx context.Context, resource *Task) error {
-	return nil
-}
+/*End of index function'*/
+
+/*UniqueIndex functions*/
+/*End of UniqueIndex functions*/
