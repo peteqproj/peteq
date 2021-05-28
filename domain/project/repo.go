78e7eef372914de426ef9960cb5baf74f96d5469
Oleg Sucharevich <oleg2807@gmail.com>
2021-05-28 18:50:36 +0300

    fix: init postgres script

--- domain/project/repo.go
+++ domain/project/repo.go
@@ -54,8 +54,8 @@ root:
 aggregates: []
 `
 var queries = []string{
-	"CREATE TABLE IF NOT EXISTS project( id text not null,userid text not null,info json not null,PRIMARY KEY (id));",
-	"CREATE INDEX IF NOT EXISTS userid ON project ( userid);",
+	"CREATE TABLE IF NOT EXISTS projects( id text not null,userid text not null,info json not null,PRIMARY KEY (id));",
+	"CREATE INDEX IF NOT EXISTS userid ON projects ( userid);",
 }
 
 type (
