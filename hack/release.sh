149d9b121a350df0decb16a75be5ca9d8b72d7f5
Oleg Sucharevich <oleg2807@gmail.com>
2021-05-29 14:14:23 +0300

    ci: update release script

--- hack/release.sh
+++ hack/release.sh
@@ -1,11 +1,18 @@
 
 #!/bin/bash
+# uses github.com/davidrjonas/semver-cli
 
 set -e
 
 version=$(cat VERSION)
-echo "Releasing version $version"
-fqrn="v$version"
+echo "Previous version: $version"
+minor=$(semver-cli inc minor $version)
+echo $minor > VERSION
+
+echo "Releasing version $minor"
+git add VERSION
+git commit -m "chore(release): version $minor"
+fqrn="v$minor"
 git checkout -b release-$fqrn
 git tag $fqrn
 git push --tags 
